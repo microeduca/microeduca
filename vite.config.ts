52064317b55e983d69583ea028127122a01a1b60
ramoncampelodeq <ramoncampelo@gmail.com>
2025-10-08 11:00:33 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,19 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  build: {
+    sourcemap: true,
+    minify: 'terser',
+    target: 'es2018',
+    terserOptions: {
+      compress: {
+        ecma: 2020,
+        inline: 0,
+        reduce_vars: false,
+      },
+      mangle: true,
+    },
+  },
   server: {
     host: "::",
     port: 8080,
